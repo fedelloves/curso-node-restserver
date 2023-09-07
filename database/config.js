951d4b61f59b14264bdf6453031c9d9d5533f0d8
Federico Lloves <fede.lloves@gmail.com>
2023-09-06 23:12:27 -0300

    Saco options de config mongoose

--- database/config.js
+++ database/config.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect( process.env.MONGODB_ATLAS, {
-            useNewUrlParser: true,
-            //useUnifiedTopology: true,
-            //useCreateIndex: true,
-            //useFindAndModify: false
-        });
+        await mongoose.connect( process.env.MONGODB_ATLAS, 
+        // {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        //     useCreateIndex: true,
+        //     useFindAndModify: false
+        // }
+        );
         console.log('Base de datos online');
     } catch (error) {
         console.log(error);
